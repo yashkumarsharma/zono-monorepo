0f62faabb067b8d302c46e49adf3887f2002cf7a
Yash Kumar Sharma <yks.nit@gmail.com>
2020-10-17 13:48:22 +0530

    feat: Added shared component workspace

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Hello from '@zono/components/HelloWeb'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
